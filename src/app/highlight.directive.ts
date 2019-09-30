1ec654baf2b0f8ff525b9e6227be9a52ca13c24c
Ngina07 <phillis.njoroge@strathmore.edu>
2019-09-30 14:01:28 +0300

    directive highlights repo name on mouse enter

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private elem: ElementRef) { }
 
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('azure');
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
+  }
+
+  private highlight(color: string) {
+    this.elem.nativeElement.style.backgroundColor = color;
+  }
 }
