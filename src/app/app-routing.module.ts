22f213f2fc900be417d2706ff4fd03f553d9e405
Ngina07 <phillis.njoroge@strathmore.edu>
2019-09-30 10:47:45 +0300

    add components to routing model so user can navigate from page to page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+ { path: 'search', component: SearchComponent },
+ { path: 'userprofile', component: UserProfileComponent},
+ { path: '', redirectTo: "/search", pathMatch: "full" },
+ { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+ imports: [RouterModule.forRoot(routes)],
+ exports: [RouterModule]
 })
 export class AppRoutingModule { }
