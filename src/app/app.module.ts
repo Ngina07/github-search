8875c6cd346ff36b314ba79a526037e0ee9907f6
Ngina07 <phillis.njoroge@strathmore.edu>
2019-09-30 10:34:07 +0300

    add user and repo classes to specify user data being displayed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+// import { HttpModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { UserProfileService } from './user-profile.service';
+import { SearchComponent } from './search/search.component';
+import { HttpModule } from '@angular/http';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserProfileComponent,
+    SearchComponent,
+    NotFoundComponent,
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpModule,
+    FormsModule
   ],
   providers: [UserProfileService],
   bootstrap: [AppComponent]
---
+++ src/app/repo.ts
@@ -0,0 +1,9 @@
+export class Repo {
+    constructor(
+      public login: string,
+      public avatar_url: string,
+      public email: string,
+      public public_repos: string,
+      public date:Date
+    ) { }
+}
