4da17f54bf1a107a9bcdd276349f067326a9dea4
Ngina07 <phillis.njoroge@strathmore.edu>
2019-09-30 10:41:45 +0300

    add form-field to search github users

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { UserProfileService } from '../user-profile.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  username: "";
+  user: any;
+  repos: any;
+  constructor(
+    private gitHubService: UserProfileService
+  ) {
+  }
+  ngOnInit() {
+  }
+  searchUser(user) {
+    console.log('user search----', user)
+    this.getUserDetails(user)
+  }
+  getUserDetails(user) {
+    this.gitHubService.getGithubUser(user).then((response) => {
+      this.user = response
+    })
+    this.gitHubService.getGithubRepos(user).then((response) => {
+      console.log('-------===00', response)
+      this.repos = response
+    })
+  }
+}
